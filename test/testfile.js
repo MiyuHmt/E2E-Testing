a6a16707a50f6f8d568f4bac612a6a1f9664d952
Marine Hemmert <marine.hemmert@gmail.com>
2017-02-14 15:57:25 +0100

    more casperjs tests

--- test/testfile.js
+++ test/testfile.js
@@ -1,29 +1,29 @@
-var url = '#'; // url
-var siteName = 'Todo';
+var url = 'http://todomvc.com/examples/angularjs/#/';
+var siteName = 'AngularJS • TodoMVC';
 var firstItem = 'Buy chocolate';
+
 casper.test.begin("Basic tests", function suite(test) {
     casper.start(url, function() {
         test.assertTitle(siteName, "This page has the correct title !");
         test.assertVisible('footer');
-        test.assertExists('h1',"H1 title is here");
-        test.assertElementCount('.ng-binding', 1);
+        test.assertExists('h3',"H3 title is here");
+        test.assertElementCount('.quote', 1);
         test.assertTextExists('todo', "page contains 'todo' ");
-
-        //this.click('#toggle-all');
+        // adding new item
+        his.sendKeys('#new-todo', firstItem);
+        this.page.sendEvent('keypress', this.page.event.key.Enter);
+        this.capture('adding.png');
 
         // asynchronous mode
-        this.click('#new-todo');
+        this.click("#new-todo");
 
         casper.then(function() {
-            test.assertElementCount('li', 3);
+            test.assertElementCount('li', 16);
             this.sendKeys('#new-todo', "Toto");
             this.page.sendEvent('keypress', this.page.event.key.Enter);
         })
-
-
     }).run(function(){
         test.done();
     });
 });
-
 
