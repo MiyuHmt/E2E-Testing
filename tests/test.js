07b05cc1cbbb65d0f434a6cad93895556ff33cab
Marine Hemmert <marine.hemmert@gmail.com>
2017-02-14 11:21:26 +0100

    More nightwatch test

--- tests/test.js
+++ tests/test.js
@@ -30,5 +30,60 @@ module.exports = {
             .assert.containsText('#todo-list li:nth-child(2) label', "Eggs")
             .saveScreenshot("./screenshot/todo-2items.png")
             .end();
+   },
+   "Checking a todolist item" : function(client)
+   {
+       client
+            .url("http://todomvc.com/examples/angularjs/#/")
+            .waitForElementVisible("#todoapp", 1000)
+            .setValue('#new-todo', 'Flour')
+            .submitForm('#todo-form')
+            .assert.containsText('#todo-list li:first-child label', "Flour")
+            .click('#todo-list li:first-child .toggle')
+            .saveScreenshot("./screenshot/todo-checked-item.png")
+            .end();
+   },
+   "Completed item list" : function(client)
+   {
+       client
+            .url("http://todomvc.com/examples/angularjs/#/")
+            .waitForElementVisible("#todoapp", 1000)
+            .setValue('#new-todo', 'Sugar')
+            .submitForm('#todo-form')
+            .assert.containsText('#todo-list li:first-child label', "Sugar")
+            .setValue('#new-todo', 'Butter')
+            .submitForm('#todo-form')
+            .click('#todo-list li:first-child .toggle')
+            .click('#filters li:nth-child(3) a')
+            .assert.containsText('#todo-list li:first-child label', 'Sugar')
+            .saveScreenshot('./screenshot/todo-Completed-list.png')
+            .end();
+   },
+   "clear completed items is visible if there is a completed item in the todo list" : function(client)
+   {
+       client
+           .url("http://todomvc.com/examples/angularjs/#/")
+            .waitForElementVisible("#todoapp", 1000)
+            .setValue('#new-todo', 'Sugar')
+            .submitForm('#todo-form')
+            .assert.hidden('#clear-completed')
+            .click('#todo-list li:first-child .toggle')
+            .assert.visible('#clear-completed')
+            .saveScreenshot('./screenshot/todo-clear-completed-button-visible.png')
+            .end();
+   },
+   "Test to add blank item in the todo list" : function(client)
+   {
+       client
+            .url("http://todomvc.com/examples/angularjs/#/")
+            .waitForElementVisible("#todoapp", 1000)
+            .setValue('#new-todo', 'Chocolate')
+            .submitForm('#todo-form')
+            .setValue('#new-todo', ' ')
+            .submitForm('#todo-form')
+            .assert.containsText('#todo-count', '1 item left')
+            .saveScreenshot('./screenshot/todo-add-blank-item.png')
+            .end();
+
    }
 };
